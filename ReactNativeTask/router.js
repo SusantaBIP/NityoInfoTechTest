2299aed5bd74669a2744bb3ccbb540dce969e0bc
Susanta Sahu <susantasahu11@gmail.com>
2021-08-28 00:27:51 +0530

    deleted

--- ReactNativeTask/router.js
+++ ReactNativeTask/router.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import {View} from 'react-native'
-import { Router, Scene } from 'react-native-router-flux'
-import songList from './src/components/songList'
-import songDetails from './src/components/songDetails';
+import React from 'react';
+import {View} from 'react-native';
+import {Router, Scene} from 'react-native-router-flux';
 import DashBoard from './src/components/DashBoard';
 
 const Routes = () => (
-   <Router>
-      <Scene key = "root">
-         <Scene key = "songList" component = {songList} title = "Song List" />
-         <Scene key = "songDetails" component = {songDetails} title = "Song Details" back={true}/>
-         <Scene key = "DashBoard" component = {DashBoard} initial = {true} hideNavBar={true}/>
-      </Scene>
-   </Router>
-)
-export default Routes
+  <Router>
+    <Scene key="root">
+      <Scene
+        key="DashBoard"
+        component={DashBoard}
+        initial={true}
+        hideNavBar={true}
+      />
+    </Scene>
+  </Router>
+);
+export default Routes;
